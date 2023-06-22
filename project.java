77cb377f4e864a28b3d7f6226d8391de625af109
Akash Kant <122162267+Akash1221@users.noreply.github.com>
2023-06-22 11:09:28 +0530

    Add files via upload

--- project.java
+++ project.java
@@ -1,6 +1,9 @@
+
 import java.util.*;
 
 import java.util.Random;
+
+// Create a class game , which allow the user to play "guess the number"
 
 class Game {
     public int number;
@@ -17,23 +20,30 @@ class Game {
         this.noOfGuesses = noOfGuesses;
     }
 
+    // 1. Consttructor to generate thw random number
+
     Game() {
         Random rnd = new Random();
         this.number = rnd.nextInt(100);
 
     }
 
+    // 2. takeUserInput() to take a input of the
+
     void takeUserInput() {
         System.out.println("Guess the number");
         Scanner sc = new Scanner(System.in);
         inputNumber = sc.nextInt();
-        // return inputNumber;
 
     }
+    // 3. isCorrectNumber() to detect whether the number entered by user is true
 
     boolean isCorrectNumber() {
         noOfGuesses++;
         if (inputNumber == number) {
+
+            // format is used for to showing the guessed number and number of attempts
+
             System.out.format("The number is correct , it was %d\n you Guessed it in %d attempts  ", number,
                     noOfGuesses);
             return true;
@@ -49,19 +59,12 @@ class Game {
 
 public class project {
     public static void main(String[] args) {
-        /*
-         * Create a class game , which allow the user to play "guess the number"
-         * 1. Consttructor to generate thw random number
-         * 2. takeUserInput() to take a input of the number
-         * 3. isCorrectNumber() to detect whether the number entered by user is true
-         * 4. getter and setter for noOfGuesses
-         * use the properties such as noOfGuesses(int),
-         */
+
         Game gm = new Game();
-        boolean b = false;
-        while (!b) {
+        boolean bool = false;
+        while (!bool) {
             gm.takeUserInput();
-            b = gm.isCorrectNumber();
+            bool = gm.isCorrectNumber();
 
         }
     }
